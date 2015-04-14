ffdb129dee35dd041ee9e25ac66c4a65d829b4ad
Jon Edwards <jkedwards@me.com>
2015-04-14 14:38:56 -0700

    almost out of time

--- client/index.js
+++ client/index.js
@@ -1,2 +1,64 @@
 'use strict';
-console.log('apollo -> online');
+
+$(document).ready(init);
+
+function init(){
+  $('#getnums').click(getRandomNums);
+  $('.even').click(squareEvenNums);
+}
+
+function getRandomNums(){
+  var arrayOfNums = [];
+  $.getJSON('https://qrng.anu.edu.au/API/jsonI.php?length=20&type=uint16', function(response){
+    arrayOfNums = response.data;
+    filterNums(arrayOfNums);
+  });
+}
+
+function filterNums(array){
+  var evensArray = [], threesArray = [], othersArray = [];
+  array.forEach(function(num){
+    if(num%2 === 0){
+      evensArray.push(num);
+    }
+    else{
+      if(num%3 === 0){
+        threesArray.push(num);
+      }
+      else{
+        othersArray.push(num);
+      }
+    }
+  });
+  var threesString = threesArray.join(', ');
+  var othersString = othersArray.join(', ');
+  displayNums(evensArray, threesString, othersString);
+}
+
+function squareEvenNums(){
+  $(this).text(Math.sqrt($(this).text()));
+}
+
+function displayNums(evensArray, threes, others){
+  // evens
+  //var $outerDiv = '<div>';
+  evensArray.forEach(function(num){
+    var $div = '<div>';
+    $($div).text(num);
+    $($div).addClass('even');
+    $(''#evens').append($div);
+  });
+  //$('#evens').append($outerDiv);
+
+  //odds
+  var $div = '<div>';
+  $($div).text(threes);
+  $('#threes').append(threes);
+
+  //others
+  var $div = '<div>';
+  $($div).text(others);
+  $('#others').append(others);
+
+
+}
